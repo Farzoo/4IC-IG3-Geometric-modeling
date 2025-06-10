6c6ca4dafef63d4f7984c9dbeedb98e7ad914f3c
Diogo <diogo95400ribeiro@gmail.com>
2025-06-10 21:38:14 +0200

    TP1 Exercice 1.2

--- Assets/Scripts/MakeMaillage.cs
+++ Assets/Scripts/MakeMaillage.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace TP1
 {
@@ -19,7 +20,7 @@ namespace TP1
                 return;
             }
     
-            this.mesh = this.MakeQuad();
+            this.mesh = this.MakeQuadLowLevel();
             
             this.meshFilter.mesh = this.mesh;
             
@@ -64,6 +65,47 @@ namespace TP1
             mesh.RecalculateBounds();
             mesh.RecalculateNormals();
     
+            return mesh;
+        }
+
+        private Mesh MakeQuadLowLevel()
+        {
+            Mesh.MeshDataArray meshData = Mesh.AllocateWritableMeshData(1);
+            
+            Mesh.MeshData meshData0 = meshData[0];
+            
+            Vector3[] vertices = new Vector3[4]
+            {
+                new Vector3(-1, 0, -1),
+                new Vector3(1, 0, -1),
+                new Vector3(1, 0, 1),
+                new Vector3(-1, 0, 1)
+            };
+
+            int[] indices = new int[6]
+            {
+                2, 1, 0,
+                3, 2, 0
+            };
+
+            meshData0.SetVertexBufferParams(vertices.Length,
+                new VertexAttributeDescriptor(VertexAttribute.Position, VertexAttributeFormat.Float32, 3));
+
+            meshData0.SetIndexBufferParams(indices.Length, IndexFormat.UInt32);
+
+            meshData0.GetVertexData<Vector3>().CopyFrom(vertices);
+            meshData0.GetIndexData<int>().CopyFrom(indices);
+            
+            meshData0.subMeshCount = 1;
+            meshData0.SetSubMesh(0, new SubMeshDescriptor(0, indices.Length, MeshTopology.Triangles));
+            
+            Mesh mesh = new Mesh();
+            
+            Mesh.ApplyAndDisposeWritableMeshData(meshData, mesh);
+            
+            mesh.RecalculateBounds();
+            mesh.RecalculateNormals();
+            
             return mesh;
         }
 
